bce2fa94fa1cf82b53af918092fa5c2454f09034
Markus <sormar14@student.westerdals.no>
2015-03-12 11:12:49 +0100

    Update Tabeller.sql
    Lagt inn kommentarer!

--- Tabeller.sql
+++ Tabeller.sql
@@ -1,13 +1,17 @@
+/* 1.utkast av databasesystem til nettsiden
+Skrevet av Markus Sørensen 11.03.2015 */
+
+
+/* Lager en tabell, rom, som inneholder informasjon om grupperommene */
 create table Rom
 (
 Id int NOT NULL,
 Antall Varchar(1) NOT NULL,
 Prosjektor Varchar(3) NOT NULL,
-
 constraint Rom_Id_Pk Primary Key Rom(Id)
 );
 
-
+/* Lager en tabell, student, som inneholder navn knyttet til en studentmail */
 create table Student
 (
 Mail Varchar(100) NOT NULL,
@@ -16,6 +20,7 @@ constraint Student_Mail_Pk Primary Key Student(Mail)
 );
 
 
+/* Lager tabell, booking, som tar imot verdier fra rom og student og oppretter en reservasjon med egen ID og dato */
 create table Booking
 (
 Id int NOT NULL auto_increment,
@@ -28,12 +33,16 @@ constraint Booking_Rom_Id_Fk Foreign Key Booking(Rom_Id) references Rom(Id)
 );
 
 
+/* Testverdier for Rom-tabellen */
 Insert into Rom (Id, Antall, Prosjektor)
 VALUES (1, 2, 'Nei'), (2, 2, 'Ja'), (3, 3, 'Nei'), (4, 3, 'Ja'), (5, 4, 'Nei'), (6, 4, 'Ja');
 
-Insert into Student (Mail, Navn)
+/* Testverdier for Student-tabellen */
+Insert into Student (Mail, Navn) 
 VALUES ('navn@navnesen.com', 'Navn Navnesen'), ('Ola@nordmann.no', 'Ola Nordmann')
 
-Insert into Booking (Dato, Mail, Rom_Id) VALUES ('01.01.2001', Mail, Rom_Id)
+/* Testverdier for Booking-tabellen */
+Insert into Booking (Dato, Mail, Rom_Id) 
+VALUES ('01.01.2001', Mail, Rom_Id)
 Select ins.Id, Mail FROM Student JOIN Booking On ins.Mail = Student.Mail,
 Select ins.Id, Rom.Id FROM Rom JOIN Booking On ins.Rom_Id = Rom.Id;
