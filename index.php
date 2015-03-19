da22531a118b3db69e45059d3fb7c36dd6c1e6f6
Racksay <edvin.klaebo@gmail.com>
2015-03-19 14:49:18 +0100

    php title

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <html>
 	<head>
-		<title>Westerdals WOACT Rom Reservasjon</title>
+		<title>WOACT Romreservasjon</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
